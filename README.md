b390d5f91cbee2205348edb9091985c7570cf5b6
nielsc <niels.claeys@gmail.com>
2024-12-23 23:01:16 +0100

    add solution day 21

--- README.md
+++ README.md
@@ -15,3 +15,8 @@ This day I struggled with the wording of peers. I initially interpreted as both 
 Based on the example it however only looked at the level, which is why I only considered that without checking they have the same manager.
 For me the wording is ambiguous, not sure what to make from it.
 
+## Day20
+
+Here the instructions are also a bit ambiguous in my opinion. We need to look for the distinct keys in the key-value pairs.
+First attempt returned an url which used twice the same key (utm_source) which is why it was not correct.
+
---
+++ day21.sql
@@ -0,0 +1,8 @@
+with extracted_quarter as (select EXTRACT(YEAR from sale_date) as year, case
+    when EXTRACT(MONTH from sale_date)  > 0 and EXTRACT(MONTH from sale_date) < 4 then 1
+    when EXTRACT(MONTH from sale_date)  > 3 and EXTRACT(MONTH from sale_date) < 7 then 2
+    when EXTRACT(MONTH from sale_date)  > 6 and EXTRACT(MONTH from sale_date) < 10 then 3
+    when EXTRACT(MONTH from sale_date)  > 9 and EXTRACT(MONTH from sale_date) < 13 then 4
+end as q, SUM(amount) as quarter_result from sales group by year, q order by year,q),
+with_previous_amount as (select year, q, quarter_result, lag(quarter_result) over(order by year, q) as previous_quarter from extracted_quarter)
+select year, q, quarter_result, previous_quarter, (quarter_result - previous_quarter)/previous_quarter as growth_rate from with_previous_amount order by growth_rate desc NULLS last;
