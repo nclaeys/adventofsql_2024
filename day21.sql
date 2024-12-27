2339d77f7511e4e6c45b4052fb897139b8b8743a
nielsc <niels.claeys@gmail.com>
2024-12-27 19:51:13 +0100

    fix day 21

--- day21.sql
+++ day21.sql
@@ -5,4 +5,4 @@ with extracted_quarter as (select EXTRACT(YEAR from sale_date) as year, case
     when EXTRACT(MONTH from sale_date)  > 9 and EXTRACT(MONTH from sale_date) < 13 then 4
 end as q, SUM(amount) as quarter_result from sales group by year, q order by year,q),
 with_previous_amount as (select year, q, quarter_result, lag(quarter_result) over(order by year, q) as previous_quarter from extracted_quarter)
-select year, q, quarter_result, previous_quarter, (quarter_result - previous_quarter)/previous_quarter as growth_rate from with_previous_amount order by growth_rate desc NULLS last;
+select year, q, quarter_result, previous_quarter, (quarter_result - previous_quarter)/previous_quarter as growth_rate from with_previous_amount order by growth_rate desc NULLS last limit 1;
