90e69633552a26c581c8101e97e230d858344404
nielsc <niels.claeys@gmail.com>
2024-12-27 20:19:03 +0100

    add limit to query

--- day13.sql
+++ day13.sql
@@ -1,3 +1,3 @@
 with emails as (select unnest(email_addresses) as email from contact_list)
 select split_part(email, '@', 2) as domain, count(1) as count from emails group by domain order by count desc
-;
+ limit 1;
